e261f3f8d87d2544fb2fa2af363ae0cb8752e232
Daniel Robertson <danielrobertson733@gmail.com>
2022-01-31 17:57:34 -0600

    increase keyboard size

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 import { SpecialKeys } from "./constants";
 import { deleteKeySvg } from "./DeleteKey";
 
@@ -17,19 +18,28 @@ export default function Keyboard({ onKey }: Props) {
 
   return (
     <div className="mt-8">
-      {keyRows.map((row, rowIdx) => (
-        <div className="flex justify-center mt-1" key={`row-${rowIdx}`}>
-          {row.map((key, idx) => (
-            <button
-              className="letter bg-gray-600 px-2 py-2 mx-0.5 rounded-sm uppercase active:bg-gray-700"
-              onClick={handleClick}
-              key={`key-${idx}`}
-            >
-              {key}
-            </button>
-          ))}
-        </div>
-      ))}
+      {keyRows.map((row, rowIdx) => {
+        const classes = classNames(
+          { "mx-3": rowIdx === 1 }, // indent second keyboard row like irl
+          "flex justify-center mt-1"
+        );
+
+        return (
+          <div className={classes} key={`row-${rowIdx}`}>
+            {row.map((key, idx) => {
+              return (
+                <button
+                  className="grow letter bg-gray-600 px-2 py-3 mx-0.5 rounded-sm uppercase active:bg-gray-700"
+                  onClick={handleClick}
+                  key={`key-${idx}`}
+                >
+                  {key}
+                </button>
+              );
+            })}
+          </div>
+        );
+      })}
       <style jsx>{`
         .letter {
           min-width: 31px;
